2cac45b63347514a4b4833fb90a020e7feb1607b
Dhruv Upadhyay <dmupadhyay12@gmail.com>
2020-03-20 17:01:22 -0400

    Added transfer learning functinality

--- dogs_cats.py
+++ dogs_cats.py
@@ -1,14 +1,11 @@
 import shutil, os
-from keras.preprocessing.image import ImageDataGenerator
-from keras import layers, models
-import matplotlib.pyplot as plt
 
 # orig_dir contains original files
 orig_dir = '/content/drive/My Drive/train/'
 
 # gen_dir will contain only disposable generated stuff, as it will be removed on
 # each run
-gen_dir = '/content/_gen/' 
+gen_dir = '/content/drive/My Drive/_gen/' 
 shutil.rmtree(gen_dir, ignore_errors=True) #This recursively removes previous instances of all gen_dir files
 os.makedirs(gen_dir) #This creates a new gen_dir directory
 
@@ -28,28 +25,34 @@ for thing in ["cat", "dog"]: #This partitions all of the files based on their la
     print(out) #Just for reference purposes
     for fn in [f"{thing}.{i}.jpg" for i in nums]:
       #Copies the files in the form cat.0.jpg (or dog.0.jpg) where the 0 is any number from the train file in My Drive to the directories established above
-      shutil.copyfile(os.path.join(orig_dir, fn), os.path.join(out, fn)) 
-      
+      shutil.copyfile(os.path.join(orig_dir, fn), os.path.join(out, fn))
+
+from keras.preprocessing.image import ImageDataGenerator
+from keras import layers, models
+import matplotlib.pyplot as plt
+from keras.applications import MobileNet
+from keras.optimizers import Adam
+
 #Adds the layers for the neural network
-model = models.Sequential()
-model.add(layers.Conv2D(32, (3,3), activation='relu', input_shape = (150,150,3)))
-model.add(layers.MaxPooling2D((2,2)))
-model.add(layers.Conv2D(64, (3,3), activation='relu'))
-model.add(layers.MaxPooling2D((2,2)))
-model.add(layers.Conv2D(128, (3,3), activation='relu'))
-model.add(layers.MaxPooling2D((2,2)))
-model.add(layers.Conv2D(128, (3,3), activation='relu'))
-model.add(layers.MaxPooling2D((2,2)))
-model.add(layers.Flatten())
-model.add(layers.Dropout(0.5));
-model.add(layers.Dense(512, activation='relu'))
-model.add(layers.Dense(1, activation='sigmoid'))
 
-model.compile(optimizer='rmsprop', loss='binary_crossentropy', metrics=['accuracy'])
+def create_model():
+  model = models.Sequential()
+  model.add(MobileNet(weights='imagenet', include_top=False))
+  model.add(layers.GlobalAveragePooling2D())
+  model.add(layers.Dropout(rate=0.5))
+  model.add(layers.Dense(1024, activation='relu'))
+  model.add(layers.Dense(1024, activation='relu'))
+  model.add(layers.Dense(512, activation='relu'))
+  model.add(layers.Dense(1, activation='sigmoid'))
+  model.compile(optimizer='Adam', loss='binary_crossentropy', metrics=['accuracy'])
 
-train_dir = '/content/_gen/train_dir/'
-val_dir = '/content/_gen/val_dir/'
-test_dir = '/content/_gen/test_dir/'
+  for layer in model.layers:
+    layer.trainable = True
+  return model
+
+train_dir = '/content/drive/My Drive/_gen/train_dir/'
+val_dir = '/content/drive/My Drive/_gen/val_dir/'
+test_dir = '/content/drive/My Drive/_gen/test_dir/'
 train_datagen = ImageDataGenerator(rescale=1./255,
                                    #The factors below are used for data augmentation
                                    #Which essentially does a series of transformations 
@@ -65,29 +68,34 @@ val_datagen = ImageDataGenerator(rescale=1./255)
 test_datagen = ImageDataGenerator(rescale=1./255)
 
 train_generator = train_datagen.flow_from_directory(train_dir, 
-                                                    target_size=(150,150),
+                                                    target_size=(224,224),
                                                     batch_size=32, 
-                                                    class_mode='binary')
+                                                    class_mode='binary',
+                                                    shuffle=True)
 validation_generator = val_datagen.flow_from_directory(val_dir,
-                                                        target_size=(150,150),
+                                                        target_size=(224,224),
                                                         batch_size=32,
-                                                        class_mode='binary')
+                                                        class_mode='binary',
+                                                       shuffle=True)
 
 test_generator = test_datagen.flow_from_directory(test_dir,
-                                                  target_size=(150,150),
+                                                  target_size=(224,224),
                                                   batch_size=32,
-                                                  class_mode='binary')
+                                                  class_mode='binary',
+                                                  shuffle=True)
+
+model = create_model()
+
+print(model.summary())
 
 history = model.fit_generator(
     train_generator, 
     steps_per_epoch=100, 
-    epochs=23,
+    epochs=7,
     validation_data = validation_generator,
     validation_steps=50)
 
 model.save('cats_and_dogs_small_1.h5')
-
-#A script that uses matplotlib to plot training vs validation loss and accuracy to effectively train the network
 
 train_acc = history.history['acc']
 val_acc = history.history['val_acc']
